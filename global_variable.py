4a265595c8f9487b261405977b45d3cb3969cdcd
摆烂Jo <32508140+zhouajiew@users.noreply.github.com>
2025-12-16 16:13:38 +0800

    Update global_variable.py

--- global_variable.py
+++ global_variable.py
@@ -1,2 +1,29 @@
+# 键盘事件监听
+listen_status = [0]
+
+# 是否暂停
+pause = [0]
+
 model = ['']
 api_key = ['']
+
+# 自动点赞视频
+min_click_like_count = [20]
+max_click_like_count = [30]
+
+# 自动评论视频
+min_comment_count = [20]
+max_comment_count = [30]
+click_like_before_comment = [0]
+
+# 自动暂停程序
+pause_automatically = [0]
+set_pause_running_time = [999999]
+
+# 自动关闭程序
+close_automatically = [0]
+set_close_running_time = [999999]
+
+# 评论失败处理
+comment_next_if_failed = [0]
+comment_next_and_click_like = [0]
